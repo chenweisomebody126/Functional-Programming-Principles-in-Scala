41fa635f3235b439768fe60dab25c054c4cdf606
Wei Chen <wei@iterable.com>
2018-03-20 17:50:06 -0700

    add: assignment accomplishment for refun

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,38 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      if ((c == 0) || (c == r)) 1
+      else pascal(c-1, r-1) + pascal(c, r-1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def isBalance(chars: List[Char], num_l : Int, num_r : Int): Boolean = {
+        if (chars.isEmpty)
+          num_l == num_r
+        else if (chars.head.equals('(') )
+          isBalance(chars.tail, num_l+1, num_r)
+        else if (chars.head.equals(')')) {
+          if (num_l - num_r - 1 < 0)
+            false
+          else
+            isBalance(chars.tail, num_l, num_r+1)
+        }
+        else
+          isBalance(chars.tail, num_l, num_r)
+      }
+      isBalance(chars, 0, 0)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0)  1
+      else countChange(money, coins.tail) + countChange(money- coins.head, coins)
+    }
   }
