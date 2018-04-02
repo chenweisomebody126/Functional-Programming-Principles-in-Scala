3b655bda840b11802a281a3ad9545ad5adf372e8
Wei Chen <wei@iterable.com>
2018-04-02 11:31:31 -0700

    add: functions and tests in the FunSets

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -19,31 +19,34 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = ???
-  
+  def singletonSet(elem: Int): Set = {
+    x:Int =>  x == elem: Boolean
+  }
+
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = ???
+    def union(s: Set, t: Set): Set = (elem: Int) => s(elem) || t(elem)
+
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = ???
+    def intersect(s: Set, t: Set): Set = (elem: Int) => s(elem) && t(elem)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = ???
+    def diff(s: Set, t: Set): Set = (elem: Int) => s(elem) && !t(elem)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = ???
+    def filter(s: Set, p: Int => Boolean): Set = (elem: Int) => s(elem) && p(elem)
   
 
   /**
@@ -54,26 +57,29 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
-    def forall(s: Set, p: Int => Boolean): Boolean = {
+  def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a < -bound ) true
+      else if ( contains(s, a) && !p(a)) false
+      else iter(a-1)
     }
-    iter(???)
+    iter(bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (elem: Int) => (!p(elem): Boolean))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
-  
+    def map(s: Set, f: Int => Int): Set = {
+      (x: Int) => exists(s, (elem: Int) => (f(elem) == x : Boolean))
+    }
+
+
   /**
    * Displays the contents of a set
    */
--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -29,10 +29,10 @@ class FunSetSuite extends FunSuite {
   /**
    * Tests are written using the "test" operator and the "assert" method.
    */
-  // test("string take") {
-  //   val message = "hello, world"
-  //   assert(message.take(5) == "hello")
-  // }
+   test("string take") {
+     val message = "hello, world"
+     assert(message.take(5) == "hello")
+   }
 
   /**
    * For ScalaTest tests, there exists a special equality operator "===" that
@@ -43,9 +43,9 @@ class FunSetSuite extends FunSuite {
    * Try it out! Change the values so that the assertion fails, and look at the
    * error message.
    */
-  // test("adding ints") {
-  //   assert(1 + 2 === 3)
-  // }
+   test("adding ints") {
+     assert(3 + 2 === 5)
+   }
 
 
   import FunSets._
@@ -107,6 +107,68 @@ class FunSetSuite extends FunSuite {
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
+    }
+  }
+
+  test("intersect contains intersection of two sets"){
+    new TestSets {
+      val uni1 = union(s1, s2)
+      val uni2 = union(s2, s3)
+      val s = intersect(uni1, uni2)
+      assert(contains(s, 2), "Intersect (1,2) and (2,3)")
+    }
+  }
+
+  test("diff contains the elements in the first but not second"){
+    new TestSets {
+      val uni1 = union(s1, s2)
+      val uni2 = union(s2, s3)
+      val s = diff(uni1, uni2)
+      assert(contains(s, 1) && !contains(s, 2), "Diff (1,2) and (2,3)" )
+    }
+  }
+
+  test("filter contains elements satisfying p" ){
+    new TestSets {
+      val s12 = union(s1,s2)
+      val s123 = union(s12,s3)
+
+      val s = filter(s123, (x: Int) => (x<2: Boolean))
+      assert(contains(s, 1) && !contains(s, 2), "Filter {1,2,3} with condition <2")
+    }
+  }
+
+  test("forall checks every element satisfying the condition p"){
+    new TestSets {
+      val s12 = union(s1,s2)
+      val s123 = union(s12,s3)
+      def smallerThan2 =  (x: Int) => (x < 2: Boolean)
+      assert(!forall(s123,smallerThan2), "set {1,2,3} are not all smaller than 2")
+      def smallerThan4 = (x: Int) => (x < 4: Boolean)
+      printSet(s123)
+      assert(forall(s123, smallerThan4), "set {1,2,3} are all smaller than 4")
+    }
+  }
+
+  test("exist checks any element satisfying the condition p"){
+    new TestSets {
+      val s12 = union(s1,s2)
+      val s123 = union(s12,s3)
+      def largerThan2 = (x: Int) => (x > 2: Boolean)
+      assert(exists(s123, largerThan2), "set {1,2,3} has element larger than 2")
+      def largerThan3 = (x: Int) => (x > 3: Boolean)
+      assert(!exists(s123, largerThan3), "set {1,2,3} has no element larger than 3")
+    }
+  }
+
+  test("map does the mapping"){
+    new TestSets {
+      val s12 = union(s1,s2)
+      val s123 = union(s12,s3)
+      def doubleVal = (x: Int) => (2*x: Int)
+      printSet(map(s123, doubleVal))
+      println(FunSets.toString(map(s123, doubleVal)))
+      assert(FunSets.toString(map(s123, doubleVal)).equals("{2,4,6}"), "double set {1, 2, 3} equals {2,4,6} ")
     }
   }
 
