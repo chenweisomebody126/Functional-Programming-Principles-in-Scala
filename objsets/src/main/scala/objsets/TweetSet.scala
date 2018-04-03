4942295ba4215c94d3aa394324f06fdd37ff7e12
Wei Chen <wei@iterable.com>
2018-04-02 17:46:29 -0700

    add: code for filtering

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -38,10 +38,10 @@ abstract class TweetSet {
    * This method takes a predicate and returns a subset of all the elements
    * in the original set for which the predicate is true.
    *
-   * Question: Can we implment this method here, or should it remain abstract
+   * Question: Can we implement this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty())
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -107,7 +107,7 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc
   
   /**
    * The following methods are already implemented
@@ -124,9 +124,11 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+      right.filterAcc(p, left.filterAcc(p, if (p(elem)) acc.incl(elem) else acc))
+    }
+
+
   /**
    * The following methods are already implemented
    */
